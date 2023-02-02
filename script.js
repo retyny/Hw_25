7731501315364c9d1d83726df842e13722ffb959
retyny <anko-style@mail.ru>
2023-02-02 23:06:14 +0200

    ConvertCurrency

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
-function sendRequestSymbols() {
-    const base_url_symbols = 'http://data.fixer.io/api/symbols?';
-    const api_key = '9e85e542661757c7f24863714f122e24';
-    return fetch(`${base_url_symbols}access_key=${api_key}`).then(response => {
-        return response.json();
-    });
+sendRequestSymbols = () => {
+    const myHeaders = new Headers();
+    myHeaders.append("apikey", "Ddkh5XlvJKajaEh4V9NjW4oHD62J3kSh");
+
+    const requestOptions = {
+        method: 'GET',
+        redirect: 'follow',
+        headers: myHeaders
+    };
+
+    return fetch("https://api.apilayer.com/fixer/symbols", requestOptions)
+        .then(result => {
+            return result.json();
+        })
+        .catch(error => console.log('error', error));
+
 }
 
-function sendRequestConvert() {
-
+sendRequestConvert = () => {
     const fromKey = document.getElementById('from').value;
     const toKey = document.getElementById('to').value;
     const amount = document.getElementById('amount').value;
@@ -32,28 +41,26 @@ function sendRequestConvert() {
         .catch(error => console.log('error', error));
 }
 
-sendRequestSymbols()
-    .then(data => {
+sendRequestSymbols().then(data => {
         for (let key in data.symbols) {
-            if(data.symbols.hasOwnProperty(key)) {
-                const selectListFrom = document.getElementById('from');
-                const newOptionFrom = document.createElement('option');
-                newOptionFrom.value = `${key}`;
-                newOptionFrom.innerText = data.symbols[key];
-                selectListFrom.appendChild(newOptionFrom);
+             if(data.symbols.hasOwnProperty(key)) {
+        const selectListFrom = document.getElementById('from');
+        const newOptionFrom = document.createElement('option');
+        newOptionFrom.value = `${key}`;
+        newOptionFrom.innerText = data.symbols[key];
+        selectListFrom.appendChild(newOptionFrom);
 
-                const selectListTo = document.getElementById('to');
-                const newOptionTo = document.createElement('option');
-                newOptionTo.value = `${key}`;
-                newOptionTo.innerText = data.symbols[key];
-                selectListTo.appendChild(newOptionTo);
-            }
-        }
+        const selectListTo = document.getElementById('to');
+        const newOptionTo = document.createElement('option');
+        newOptionTo.value = `${key}`;
+        newOptionTo.innerText = data.symbols[key];
+        selectListTo.appendChild(newOptionTo);
+    }
+}
+}).catch(err =>console.log(err));
 
-    }).catch(err =>console.log(err));
 
-
-function sendRequestCurrencyConverter() {
+sendRequestCurrencyConverter = () => {
     sendRequestConvert();
 }
 
